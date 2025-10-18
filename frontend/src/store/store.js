eb1b6ce28c24ba86e9790f44831bdbdd1d0f43ec
Sah-Rahul <sahfamilyinfo@gmail.com>
2025-10-19 00:40:48 +0545

    setup redux persits

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,12 +1,41 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./slices/authSlice";
-import jobReducer from "./slices/jobSlice";
+import { combineReducers } from "redux";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import authSlice from  "./slices/authSlice";
+import jobSlice from "./slices/jobSlice";
+
+const rootReducer = combineReducers({
+  auth: authSlice,
+  job: jobSlice,
+});
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    job: jobReducer
-  }
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export default store;
+export const persistor = persistStore(store);
